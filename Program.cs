4f345b2d9fd3f1c23eb3ff79bc86576ae6be4da6
EuanMacLeod <em1026@student.aru.ac.uk>
2023-10-19 18:44:59 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Software_Engineering_Project_New
+
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new HomePage());
+        }
+    }
+    
+}
