bf1c4ed632ad81612375474e4f7bb47187f7bfa1
hadees-ahmed <hadeesahmed@yahoo.com>
2023-11-15 18:46:50 +0000

    enhanced the code and implemented view softwares

--- SoftwareSales.cs
+++ SoftwareSales.cs
@@ -1,87 +1,118 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Data.SqlClient;
-using System.Drawing;
-using System.Drawing.Text;
-using System.Linq;
-using System.Net.Http.Headers;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Xml.Serialization;
-using System.Data.SqlClient;
-using System.Threading;
+using System.Linq;
+
 
 namespace Software_Engineering_Project_New
 {
     public partial class SoftwareSales : Form
     {
-        int count;
+        private int count;
 
         public SoftwareSales()
         {
             InitializeComponent();
-
+            InitializeData();
         }
 
-       
-
-        private void SoftwareSales_Load(object sender, EventArgs e)
+        private void InitializeData()
         {
-            // TODO: This line of code loads data into the 'citisoftDataSet.Softwares' table. You can move, or remove it, as needed.
             this.softwaresTableAdapter.Fill(this.citisoftDataSet.Softwares);
+            DisplaySoftwareInformation();
+        }
 
-            if (this.citisoftDataSet.Softwares.Rows.Count > 0)
+        private void DisplaySoftwareInformation()
+        {
+            ClearSoftwareInfoTextBoxes();
+
+            for (int i = 0; i < 4; i++)
             {
+                int rowIndex = count + i;
 
-                
-                var Nextrow = this.citisoftDataSet.Softwares.Rows[count];
-
-                softwareNameTextBox.Text = Nextrow["Name"].ToString();
-                softwareDescriptionTextBox.Text = Nextrow["Description"].ToString();
-
-                count = count + 1;
-                Nextrow = this.citisoftDataSet.Softwares.Rows[count];
-
-                softwareNameTextBox2.Text = Nextrow["Name"].ToString();
-                softwareDescriptionTextBox2.Text = Nextrow["Description"].ToString();
-
-                count = count + 1;
-                Nextrow = this.citisoftDataSet.Softwares.Rows[count];
-
-
-                softwareNameTextBox3.Text = Nextrow["Name"].ToString();
-                softwareDescriptionTextBox3.Text = Nextrow["Description"].ToString();
-
-                count = count + 1;
-                Nextrow = this.citisoftDataSet.Softwares.Rows[count];
-
-
-                softwareNameTextBox4.Text = Nextrow["Name"].ToString();
-                softwareDescriptionTextBox4.Text = Nextrow["Description"].ToString();
-
-
-
+                if (rowIndex < this.citisoftDataSet.Softwares.Rows.Count)
+                {
+                    DataRow row = this.citisoftDataSet.Softwares.Rows[rowIndex];
+                    SetSoftwareInfoTextBoxes(i + 1, row);
+                }
+                else
+                {
+                    break;
+                }
             }
-
-
         }
 
-        private void DisplayRowInDataGridView(DataGridView dataGridView, DataTable softwareTable, int rowIndex)
+        private void SetSoftwareInfoTextBoxes(int setNumber, DataRow row)
         {
+            TextBox nameTextBox = (TextBox)this.Controls.Find($"softwareNameTextBox{setNumber}", true).FirstOrDefault();
+            TextBox descriptionTextBox = (TextBox)this.Controls.Find($"softwareDescriptionTextBox{setNumber}", true).FirstOrDefault();
 
+            if (nameTextBox != null && descriptionTextBox != null)
+            {
+                nameTextBox.Text = row["Name"].ToString();
+                descriptionTextBox.Text = row["Description"].ToString();
+            }
         }
 
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private void ClearSoftwareInfoTextBoxes()
         {
-            
+            for (int i = 1; i <= 4; i++)
+            {
+                TextBox nameTextBox = (TextBox)this.Controls.Find($"softwareNameTextBox{i}", true).FirstOrDefault();
+                TextBox descriptionTextBox = (TextBox)this.Controls.Find($"softwareDescriptionTextBox{i}", true).FirstOrDefault();
+
+                nameTextBox?.Clear();
+                descriptionTextBox?.Clear();
+            }
         }
+
 
         private void nextPagebutton_Click(object sender, EventArgs e)
         {
             count = count + 4;
+            DisplaySoftwareInformation();
         }
     }
 }
+//alex code:
+
+/*private void SoftwareSales_Load(object sender, EventArgs e)
+{
+    // TODO: This line of code loads data into the 'citisoftDataSet.Softwares' table. You can move, or remove it, as needed.
+    this.softwaresTableAdapter.Fill(this.citisoftDataSet.Softwares);
+
+    if (this.citisoftDataSet.Softwares.Rows.Count > 0)
+    {
+
+
+        var Nextrow = this.citisoftDataSet.Softwares.Rows[count];
+
+        softwareNameTextBox.Text = Nextrow["Name"].ToString();
+        softwareDescriptionTextBox.Text = Nextrow["Description"].ToString();
+
+        count = count + 1;
+        Nextrow = this.citisoftDataSet.Softwares.Rows[count];
+
+        softwareNameTextBox2.Text = Nextrow["Name"].ToString();
+        softwareDescriptionTextBox2.Text = Nextrow["Description"].ToString();
+
+        count = count + 1;
+        Nextrow = this.citisoftDataSet.Softwares.Rows[count];
+
+
+        softwareNameTextBox3.Text = Nextrow["Name"].ToString();
+        softwareDescriptionTextBox3.Text = Nextrow["Description"].ToString();
+
+        count = count + 1;
+        Nextrow = this.citisoftDataSet.Softwares.Rows[count];
+
+
+        softwareNameTextBox4.Text = Nextrow["Name"].ToString();
+        softwareDescriptionTextBox4.Text = Nextrow["Description"].ToString();
+
+
+
+    }
+
+
+}*/
