b62ffd8e42c9179257d3839e49bc791eec7d96b4
EuanMacLeod <em1026@student.aru.ac.uk>
2023-11-17 13:08:53 +0000

    added new page to view users awaitng approval

---
+++ Controllers/DatabaseEngineer/AdminTools/ApproveUser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Software_Engineering_Project_New.Controllers.DatabaseEngineer.AdminTools
+{
+    public partial class ApproveUser : Form
+    {
+        public ApproveUser()
+        {
+            InitializeComponent();
+            populateDgv(generateDataTable());
+        }
+
+
+        private void populateDgv(DataTable dt)
+        {
+            usersAwatingApproval.DataSource = dt;
+        }
+
+        private DataTable generateDataTable()
+        {
+            DataTable dt = DBConnections.getInstanceOfDBConnection().getDataTable("SELECT * FROM Employees WHERE RoleID IS NULL");
+
+            return dt;
+        }
+    }
+}
