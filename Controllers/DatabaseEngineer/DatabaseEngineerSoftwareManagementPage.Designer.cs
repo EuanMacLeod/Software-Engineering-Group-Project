c8abeca105396b3649101512772be48b661e4aed
EuanMacLeod <em1026@student.aru.ac.uk>
2023-11-27 08:33:16 +0000

    added more dgv's to view software info

--- Controllers/DatabaseEngineer/DatabaseEngineerSoftwareManagementPage.Designer.cs
+++ Controllers/DatabaseEngineer/DatabaseEngineerSoftwareManagementPage.Designer.cs
@@ -30,11 +30,24 @@
         {
             this.components = new System.ComponentModel.Container();
             this.SoftwaresDgv = new System.Windows.Forms.DataGridView();
-            this.citisoftSoftwaresDataSet = new Software_Engineering_Project_New.CitisoftSoftwaresDataSet();
             this.citisoftSoftwaresDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.citisoftSoftwaresDataSet = new Software_Engineering_Project_New.CitisoftSoftwaresDataSet();
+            this.SoftwareTypeDgv = new System.Windows.Forms.DataGridView();
+            this.SoftwareModuleDgv = new System.Windows.Forms.DataGridView();
+            this.ClientTypeDgv = new System.Windows.Forms.DataGridView();
+            this.BuisnessAreasDgv = new System.Windows.Forms.DataGridView();
+            this.SoftwaresLabel = new System.Windows.Forms.Label();
+            this.SoftwareModuleLabel = new System.Windows.Forms.Label();
+            this.ClientTypeLabel = new System.Windows.Forms.Label();
+            this.SoftwareTypeLabel = new System.Windows.Forms.Label();
+            this.BuisnessAreasLabel = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.SoftwaresDgv)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.citisoftSoftwaresDataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.citisoftSoftwaresDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.citisoftSoftwaresDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SoftwareTypeDgv)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SoftwareModuleDgv)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ClientTypeDgv)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BuisnessAreasDgv)).BeginInit();
             this.SuspendLayout();
             // 
             // SoftwaresDgv
@@ -44,39 +57,177 @@
             this.SoftwaresDgv.AutoGenerateColumns = false;
             this.SoftwaresDgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.SoftwaresDgv.DataSource = this.citisoftSoftwaresDataSetBindingSource;
-            this.SoftwaresDgv.Location = new System.Drawing.Point(0, 0);
+            this.SoftwaresDgv.Location = new System.Drawing.Point(0, 36);
+            this.SoftwaresDgv.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.SoftwaresDgv.MultiSelect = false;
             this.SoftwaresDgv.Name = "SoftwaresDgv";
             this.SoftwaresDgv.ReadOnly = true;
             this.SoftwaresDgv.RowHeadersWidth = 62;
             this.SoftwaresDgv.RowTemplate.Height = 28;
             this.SoftwaresDgv.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.SoftwaresDgv.Size = new System.Drawing.Size(1015, 512);
+            this.SoftwaresDgv.Size = new System.Drawing.Size(607, 401);
             this.SoftwaresDgv.TabIndex = 0;
-            // 
-            // citisoftSoftwaresDataSet
-            // 
-            this.citisoftSoftwaresDataSet.DataSetName = "CitisoftSoftwaresDataSet";
-            this.citisoftSoftwaresDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
             // 
             // citisoftSoftwaresDataSetBindingSource
             // 
             this.citisoftSoftwaresDataSetBindingSource.DataSource = this.citisoftSoftwaresDataSet;
             this.citisoftSoftwaresDataSetBindingSource.Position = 0;
             // 
+            // citisoftSoftwaresDataSet
+            // 
+            this.citisoftSoftwaresDataSet.DataSetName = "CitisoftSoftwaresDataSet";
+            this.citisoftSoftwaresDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // SoftwareTypeDgv
+            // 
+            this.SoftwareTypeDgv.AllowUserToAddRows = false;
+            this.SoftwareTypeDgv.AllowUserToDeleteRows = false;
+            this.SoftwareTypeDgv.AutoGenerateColumns = false;
+            this.SoftwareTypeDgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.SoftwareTypeDgv.DataSource = this.citisoftSoftwaresDataSetBindingSource;
+            this.SoftwareTypeDgv.Location = new System.Drawing.Point(611, 36);
+            this.SoftwareTypeDgv.Margin = new System.Windows.Forms.Padding(2);
+            this.SoftwareTypeDgv.MultiSelect = false;
+            this.SoftwareTypeDgv.Name = "SoftwareTypeDgv";
+            this.SoftwareTypeDgv.ReadOnly = true;
+            this.SoftwareTypeDgv.RowHeadersWidth = 62;
+            this.SoftwareTypeDgv.RowTemplate.Height = 28;
+            this.SoftwareTypeDgv.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.SoftwareTypeDgv.Size = new System.Drawing.Size(384, 176);
+            this.SoftwareTypeDgv.TabIndex = 1;
+            // 
+            // SoftwareModuleDgv
+            // 
+            this.SoftwareModuleDgv.AllowUserToAddRows = false;
+            this.SoftwareModuleDgv.AllowUserToDeleteRows = false;
+            this.SoftwareModuleDgv.AutoGenerateColumns = false;
+            this.SoftwareModuleDgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.SoftwareModuleDgv.DataSource = this.citisoftSoftwaresDataSetBindingSource;
+            this.SoftwareModuleDgv.Location = new System.Drawing.Point(611, 261);
+            this.SoftwareModuleDgv.Margin = new System.Windows.Forms.Padding(2);
+            this.SoftwareModuleDgv.MultiSelect = false;
+            this.SoftwareModuleDgv.Name = "SoftwareModuleDgv";
+            this.SoftwareModuleDgv.ReadOnly = true;
+            this.SoftwareModuleDgv.RowHeadersWidth = 62;
+            this.SoftwareModuleDgv.RowTemplate.Height = 28;
+            this.SoftwareModuleDgv.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.SoftwareModuleDgv.Size = new System.Drawing.Size(384, 176);
+            this.SoftwareModuleDgv.TabIndex = 2;
+            // 
+            // ClientTypeDgv
+            // 
+            this.ClientTypeDgv.AllowUserToAddRows = false;
+            this.ClientTypeDgv.AllowUserToDeleteRows = false;
+            this.ClientTypeDgv.AutoGenerateColumns = false;
+            this.ClientTypeDgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ClientTypeDgv.DataSource = this.citisoftSoftwaresDataSetBindingSource;
+            this.ClientTypeDgv.Location = new System.Drawing.Point(999, 261);
+            this.ClientTypeDgv.Margin = new System.Windows.Forms.Padding(2);
+            this.ClientTypeDgv.MultiSelect = false;
+            this.ClientTypeDgv.Name = "ClientTypeDgv";
+            this.ClientTypeDgv.ReadOnly = true;
+            this.ClientTypeDgv.RowHeadersWidth = 62;
+            this.ClientTypeDgv.RowTemplate.Height = 28;
+            this.ClientTypeDgv.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.ClientTypeDgv.Size = new System.Drawing.Size(384, 176);
+            this.ClientTypeDgv.TabIndex = 3;
+            // 
+            // BuisnessAreasDgv
+            // 
+            this.BuisnessAreasDgv.AllowUserToAddRows = false;
+            this.BuisnessAreasDgv.AllowUserToDeleteRows = false;
+            this.BuisnessAreasDgv.AutoGenerateColumns = false;
+            this.BuisnessAreasDgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.BuisnessAreasDgv.DataSource = this.citisoftSoftwaresDataSetBindingSource;
+            this.BuisnessAreasDgv.Location = new System.Drawing.Point(999, 36);
+            this.BuisnessAreasDgv.Margin = new System.Windows.Forms.Padding(2);
+            this.BuisnessAreasDgv.MultiSelect = false;
+            this.BuisnessAreasDgv.Name = "BuisnessAreasDgv";
+            this.BuisnessAreasDgv.ReadOnly = true;
+            this.BuisnessAreasDgv.RowHeadersWidth = 62;
+            this.BuisnessAreasDgv.RowTemplate.Height = 28;
+            this.BuisnessAreasDgv.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.BuisnessAreasDgv.Size = new System.Drawing.Size(384, 176);
+            this.BuisnessAreasDgv.TabIndex = 4;
+            // 
+            // SoftwaresLabel
+            // 
+            this.SoftwaresLabel.AutoSize = true;
+            this.SoftwaresLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SoftwaresLabel.Location = new System.Drawing.Point(0, 0);
+            this.SoftwaresLabel.Name = "SoftwaresLabel";
+            this.SoftwaresLabel.Size = new System.Drawing.Size(144, 31);
+            this.SoftwaresLabel.TabIndex = 5;
+            this.SoftwaresLabel.Text = "Softwares:";
+            // 
+            // SoftwareModuleLabel
+            // 
+            this.SoftwareModuleLabel.AutoSize = true;
+            this.SoftwareModuleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SoftwareModuleLabel.Location = new System.Drawing.Point(612, 228);
+            this.SoftwareModuleLabel.Name = "SoftwareModuleLabel";
+            this.SoftwareModuleLabel.Size = new System.Drawing.Size(239, 31);
+            this.SoftwareModuleLabel.TabIndex = 6;
+            this.SoftwareModuleLabel.Text = "Software Modules:";
+            // 
+            // ClientTypeLabel
+            // 
+            this.ClientTypeLabel.AutoSize = true;
+            this.ClientTypeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ClientTypeLabel.Location = new System.Drawing.Point(1000, 228);
+            this.ClientTypeLabel.Name = "ClientTypeLabel";
+            this.ClientTypeLabel.Size = new System.Drawing.Size(290, 31);
+            this.ClientTypeLabel.TabIndex = 7;
+            this.ClientTypeLabel.Text = "Financial Client Types:";
+            // 
+            // SoftwareTypeLabel
+            // 
+            this.SoftwareTypeLabel.AutoSize = true;
+            this.SoftwareTypeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SoftwareTypeLabel.Location = new System.Drawing.Point(612, 3);
+            this.SoftwareTypeLabel.Name = "SoftwareTypeLabel";
+            this.SoftwareTypeLabel.Size = new System.Drawing.Size(234, 31);
+            this.SoftwareTypeLabel.TabIndex = 8;
+            this.SoftwareTypeLabel.Text = "Type Of Software:";
+            // 
+            // BuisnessAreasLabel
+            // 
+            this.BuisnessAreasLabel.AutoSize = true;
+            this.BuisnessAreasLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BuisnessAreasLabel.Location = new System.Drawing.Point(1000, 3);
+            this.BuisnessAreasLabel.Name = "BuisnessAreasLabel";
+            this.BuisnessAreasLabel.Size = new System.Drawing.Size(326, 31);
+            this.BuisnessAreasLabel.TabIndex = 9;
+            this.BuisnessAreasLabel.Text = "Software Buisness Areas:";
+            // 
             // DatabaseEngineerSoftwareManagementPage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1226, 669);
+            this.ClientSize = new System.Drawing.Size(1413, 754);
+            this.Controls.Add(this.BuisnessAreasLabel);
+            this.Controls.Add(this.SoftwareTypeLabel);
+            this.Controls.Add(this.ClientTypeLabel);
+            this.Controls.Add(this.SoftwareModuleLabel);
+            this.Controls.Add(this.SoftwaresLabel);
+            this.Controls.Add(this.BuisnessAreasDgv);
+            this.Controls.Add(this.ClientTypeDgv);
+            this.Controls.Add(this.SoftwareModuleDgv);
+            this.Controls.Add(this.SoftwareTypeDgv);
             this.Controls.Add(this.SoftwaresDgv);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "DatabaseEngineerSoftwareManagementPage";
             this.Text = "DatabaseEngineerSoftwareManagementPage";
             this.Load += new System.EventHandler(this.LoadSoftwaresDgv);
             ((System.ComponentModel.ISupportInitialize)(this.SoftwaresDgv)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.citisoftSoftwaresDataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.citisoftSoftwaresDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.citisoftSoftwaresDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SoftwareTypeDgv)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SoftwareModuleDgv)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ClientTypeDgv)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BuisnessAreasDgv)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -85,5 +236,14 @@
         private System.Windows.Forms.DataGridView SoftwaresDgv;
         private System.Windows.Forms.BindingSource citisoftSoftwaresDataSetBindingSource;
         private CitisoftSoftwaresDataSet citisoftSoftwaresDataSet;
+        private System.Windows.Forms.DataGridView SoftwareTypeDgv;
+        private System.Windows.Forms.DataGridView SoftwareModuleDgv;
+        private System.Windows.Forms.DataGridView ClientTypeDgv;
+        private System.Windows.Forms.DataGridView BuisnessAreasDgv;
+        private System.Windows.Forms.Label SoftwaresLabel;
+        private System.Windows.Forms.Label SoftwareModuleLabel;
+        private System.Windows.Forms.Label ClientTypeLabel;
+        private System.Windows.Forms.Label SoftwareTypeLabel;
+        private System.Windows.Forms.Label BuisnessAreasLabel;
     }
 }
