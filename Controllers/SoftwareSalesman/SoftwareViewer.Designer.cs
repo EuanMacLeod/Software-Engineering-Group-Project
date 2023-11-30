027529668dcf2583c6397bad88f9312511694cf7
Alex Williams <Alexaldrin14@gmail.com>
2023-11-30 21:35:38 +0000

    ui update on viewer menu

--- Controllers/SoftwareSalesman/SoftwareViewer.Designer.cs
+++ Controllers/SoftwareSalesman/SoftwareViewer.Designer.cs
@@ -29,78 +29,28 @@
         private void InitializeComponent()
         {
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.ViewerDescriptionBox = new System.Windows.Forms.TextBox();
-            this.ViewerNameBox = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
             this.ViewerBackButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SoftwareNameLabel = new System.Windows.Forms.Label();
+            this.SoftwareDescLabel = new System.Windows.Forms.Label();
+            this.AddInfoLabel = new System.Windows.Forms.Label();
+            this.DemoDateLabel = new System.Windows.Forms.Label();
+            this.documentLinkLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox2
             // 
             this.pictureBox2.Image = global::Software_Engineering_Project_New.Properties.Resources.logo;
-            this.pictureBox2.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox2.Location = new System.Drawing.Point(580, 47);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(138, 126);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 21;
             this.pictureBox2.TabStop = false;
-            // 
-            // ViewerDescriptionBox
-            // 
-            this.ViewerDescriptionBox.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.ViewerDescriptionBox.Location = new System.Drawing.Point(156, 38);
-            this.ViewerDescriptionBox.Multiline = true;
-            this.ViewerDescriptionBox.Name = "ViewerDescriptionBox";
-            this.ViewerDescriptionBox.Size = new System.Drawing.Size(531, 100);
-            this.ViewerDescriptionBox.TabIndex = 20;
-            // 
-            // ViewerNameBox
-            // 
-            this.ViewerNameBox.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.ViewerNameBox.Location = new System.Drawing.Point(156, 12);
-            this.ViewerNameBox.Name = "ViewerNameBox";
-            this.ViewerNameBox.Size = new System.Drawing.Size(168, 20);
-            this.ViewerNameBox.TabIndex = 19;
-            // 
-            // textBox1
-            // 
-            this.textBox1.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.textBox1.Location = new System.Drawing.Point(12, 144);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(168, 22);
-            this.textBox1.TabIndex = 22;
-            // 
-            // textBox2
-            // 
-            this.textBox2.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.textBox2.Location = new System.Drawing.Point(12, 172);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(168, 22);
-            this.textBox2.TabIndex = 23;
-            // 
-            // textBox3
-            // 
-            this.textBox3.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.textBox3.Location = new System.Drawing.Point(12, 200);
-            this.textBox3.Multiline = true;
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(168, 22);
-            this.textBox3.TabIndex = 24;
-            // 
-            // textBox4
-            // 
-            this.textBox4.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.textBox4.Location = new System.Drawing.Point(12, 228);
-            this.textBox4.Multiline = true;
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(168, 22);
-            this.textBox4.TabIndex = 25;
             // 
             // ViewerBackButton
             // 
@@ -112,19 +62,115 @@
             this.ViewerBackButton.UseVisualStyleBackColor = true;
             this.ViewerBackButton.Click += new System.EventHandler(this.ViewerBackButton_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 256);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 16);
+            this.label1.TabIndex = 27;
+            this.label1.Text = "Date Of Demo:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 282);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 16);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "Document Link:";
+            // 
+            // SoftwareNameLabel
+            // 
+            this.SoftwareNameLabel.AutoSize = true;
+            this.SoftwareNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SoftwareNameLabel.Location = new System.Drawing.Point(17, 15);
+            this.SoftwareNameLabel.Name = "SoftwareNameLabel";
+            this.SoftwareNameLabel.Size = new System.Drawing.Size(155, 25);
+            this.SoftwareNameLabel.TabIndex = 29;
+            this.SoftwareNameLabel.Text = "Software name";
+            this.SoftwareNameLabel.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // SoftwareDescLabel
+            // 
+            this.SoftwareDescLabel.AutoSize = true;
+            this.SoftwareDescLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SoftwareDescLabel.Location = new System.Drawing.Point(19, 60);
+            this.SoftwareDescLabel.MaximumSize = new System.Drawing.Size(531, 92);
+            this.SoftwareDescLabel.Name = "SoftwareDescLabel";
+            this.SoftwareDescLabel.Size = new System.Drawing.Size(94, 16);
+            this.SoftwareDescLabel.TabIndex = 30;
+            this.SoftwareDescLabel.Text = "Software Desc";
+            // 
+            // AddInfoLabel
+            // 
+            this.AddInfoLabel.AutoSize = true;
+            this.AddInfoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddInfoLabel.Location = new System.Drawing.Point(16, 157);
+            this.AddInfoLabel.MaximumSize = new System.Drawing.Size(531, 88);
+            this.AddInfoLabel.Name = "AddInfoLabel";
+            this.AddInfoLabel.Size = new System.Drawing.Size(135, 16);
+            this.AddInfoLabel.TabIndex = 31;
+            this.AddInfoLabel.Text = "Additional information";
+            // 
+            // DemoDateLabel
+            // 
+            this.DemoDateLabel.AutoSize = true;
+            this.DemoDateLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DemoDateLabel.Location = new System.Drawing.Point(113, 256);
+            this.DemoDateLabel.Name = "DemoDateLabel";
+            this.DemoDateLabel.Size = new System.Drawing.Size(76, 16);
+            this.DemoDateLabel.TabIndex = 32;
+            this.DemoDateLabel.Text = "Demo Date";
+            // 
+            // documentLinkLabel
+            // 
+            this.documentLinkLabel.AutoSize = true;
+            this.documentLinkLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.documentLinkLabel.Location = new System.Drawing.Point(116, 282);
+            this.documentLinkLabel.Name = "documentLinkLabel";
+            this.documentLinkLabel.Size = new System.Drawing.Size(91, 16);
+            this.documentLinkLabel.TabIndex = 33;
+            this.documentLinkLabel.Text = "Document link";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(18, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 20);
+            this.label3.TabIndex = 34;
+            this.label3.Text = "Description";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(18, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 20);
+            this.label4.TabIndex = 35;
+            this.label4.Text = "Additional Info:";
+            // 
             // SoftwareViewer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.documentLinkLabel);
+            this.Controls.Add(this.DemoDateLabel);
+            this.Controls.Add(this.AddInfoLabel);
+            this.Controls.Add(this.SoftwareDescLabel);
+            this.Controls.Add(this.SoftwareNameLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.ViewerBackButton);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.ViewerDescriptionBox);
-            this.Controls.Add(this.ViewerNameBox);
             this.Name = "SoftwareViewer";
             this.Text = "SoftwareViewer";
             this.Load += new System.EventHandler(this.SoftwareViewer_Load);
@@ -137,12 +183,15 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.TextBox ViewerDescriptionBox;
-        private System.Windows.Forms.TextBox ViewerNameBox;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Button ViewerBackButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label SoftwareNameLabel;
+        private System.Windows.Forms.Label SoftwareDescLabel;
+        private System.Windows.Forms.Label AddInfoLabel;
+        private System.Windows.Forms.Label DemoDateLabel;
+        private System.Windows.Forms.Label documentLinkLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
