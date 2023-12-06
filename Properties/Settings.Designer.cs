cd23770f8011cbf5c1295752b8f22a445460077d
EuanMacLeod <em1026@student.aru.ac.uk>
2023-12-06 18:56:03 +0000

    edited settings

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -51,5 +51,16 @@ namespace Software_Engineering_Project_New.Properties {
                 return ((string)(this["EmailPassword"]));
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Citisoft.mdf;" +
+            "Integrated Security=True;Connect Timeout=30")]
+        public string CitisoftDBConnection {
+            get {
+                return ((string)(this["CitisoftDBConnection"]));
+            }
+        }
     }
 }
